1b92f04238c4d2626d39d38736a0a41836118484
Steven Soekha <steven.soekha@outlook.com>
2020-05-01 13:25:05 +0200

    Visitor class created
    All the parking entrance and exiting logic will be handled by this class.

--- api/garage/services.py
+++ api/garage/services.py
@@ -1,4 +1,6 @@
-from .models import TimeTable
+from .models import TimeTable, Car
+from django.db.models.base import ObjectDoesNotExist
+from django.http import JsonResponse
 
 class TimeTableService:
     timeTable = None
@@ -11,3 +13,65 @@ class TimeTableService:
 
     def getCheckInTime(self):
         return self.timeTable.check_in_time
+    
+    def deleteTime(self, user_id):
+        # Transfer to Transactions table
+        pass
+        
+        # Delete from table
+        self.timeTable.delete()
+
+class Visitor:
+    time_table = None
+    car = None
+    status = { 'status': None, 'error': None, 'code': 200 }
+
+    def __init__(self, license_plate):
+        if not self.isValid(license_plate):
+            return
+            
+        self.setTime(license_plate)
+        self.setCar(license_plate)
+        self.setStatus()
+    
+    def isValid(self, license_plate):
+        if not license_plate:
+            self.status['status'] = None
+            self.status['error'] = 'Je hebt geen kenteken meegegeven!'
+            self.status['code'] = 400
+            return False
+        if len(license_plate) != 8:
+            self.status['status'] = None
+            self.status['error'] = 'Dit is geen geldig kenteken!'
+            self.status['code'] = 400
+            return False
+        return True
+    
+    def setTime(self, license_plate):
+        try:
+            self.time_table = TimeTable.objects.get(car_id=license_plate)
+        except ObjectDoesNotExist as err:
+            print(f'Error message: {err}')
+    
+    def setCar(self, license_plate):
+        try:
+            self.car = Car.objects.get(license_plate_number=license_plate)
+        except ObjectDoesNotExist as err:
+            print(f'Error message: {err}')
+    
+    def setStatus(self):
+        if self.time_table and self.car:
+            # The visitor wants to go out
+            self.status['status'] = True
+            self.status['error'] = 'De bezoeker verlaat de garage.'
+        elif not self.time_table and self.car:
+            # The visitor wants to go in
+            self.status['status'] = False
+            self.status['error'] = 'De bezoeker betreedt de garage.'
+        else:
+            # The visitor does not exist
+            self.status['status'] = None
+            self.status['error'] = 'De bezoeker is geen klant.'
+    
+    def response(self):
+        return JsonResponse({ 'status': self.status['status'], 'error': self.status['error'] }, status = self.status['code'])
--- api/garage/views.py
+++ api/garage/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from django.http import JsonResponse
-from .models import Car, Garage
-from .serializers import TimeTableSerializer, GarageSerializer
-from users.services import RequestHandler, CarService, CustomerService
+from .models import Garage
+from .serializers import GarageSerializer
+from users.services import RequestHandler, CustomerService
 from rest_framework import status
-from . services import TimeTableService
-from django.contrib.auth.models import User
+from . services import TimeTableService, Visitor
 
 # TODO:
 # Handle objects that does not exists with correct error handling
@@ -39,24 +37,9 @@ def garage(request):
     else:
         return JsonResponse({'Message': 'Unauthorized access'})
 
-
-@api_view(["DELETE", "PUT", "POST"])
-@permission_classes([IsAuthenticated])
-def time_table(request):
-    if request.method == "POST":
-        serializer = TimeTableSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse({'newCar': serializer.data, 'message': 'Car has entered the garage'}, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-    if request.method == "PUT":
-        request_handel = RequestHandler(request)
-        time_table = TimeTableService(license_plate=request.data['car'], garage_id=request.data['garage'])
-        request_handel.add_to_dictionary('check_in_time', time_table.getCheckInTime())
-        serializer = TimeTableSerializer(time_table.getTimeTable(), data=request_handel.get_dictionary())
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse({'message': 'Car has left'})
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+@api_view(["POST"])
+@permission_classes([])
+def time_table(request, **kwargs):
+    license = request.GET.get('license')
+    visitor = Visitor(license)
+    return visitor.response()
