ea79aa8549d395d40889499af01e588485e61897
Steven Soekha <steven.soekha@outlook.com>
2020-05-08 22:11:14 +0200

    Update styling of console output

--- api/specialpark/tests.py
+++ api/specialpark/tests.py
@@ -2,7 +2,7 @@ from ast import literal_eval
 from simple_chalk import green
 
 def success(test_name):
-    print(f'{test_name} -', green.bold('passed'), u'\u2713')
+    print(' '*3, green.bold('passed'), green.bold(u'\u2713'), f'- {test_name}')
 
 def decode_response(res):
     return literal_eval(res.decode('utf-8'))
