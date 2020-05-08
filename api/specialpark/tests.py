dccb8c652090bbb6d144f91c34d7df5ccb4baefa
Steven Soekha <steven.soekha@outlook.com>
2020-05-08 22:16:55 +0200

    Update tests.py

--- api/specialpark/tests.py
+++ api/specialpark/tests.py
@@ -2,7 +2,7 @@ from ast import literal_eval
 from simple_chalk import green
 
 def success(test_name):
-    print(' '*3, green.bold('passed'), green.bold(u'\u2713'), f'- {test_name}')
+    print(' ', green.bold('passed'), green.bold(u'\u2713'), f'- {test_name}')
 
 def decode_response(res):
     return literal_eval(res.decode('utf-8'))
